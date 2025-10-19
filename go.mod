88b1107fb42950116314adbcdb07bd04652fa2e0
Josh Stein <josh.e.stein@gmail.com>
2025-10-19 22:34:37 +0200

    Setup initial router infra

---
+++ go.mod
@@ -0,0 +1,8 @@
+module spreadinglove
+
+go 1.24.6
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("bye"))
+	})
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: r,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Block
+	<-quit
+	log.Println("Shutdown Server ...")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
