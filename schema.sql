8dc70dd315eb37100eb91099f223e10b36e2a335
Josh Stein <josh.e.stein@gmail.com>
2025-10-21 23:10:43 +0200

    Update seed messages

--- schema.sql
+++ schema.sql
@@ -11,7 +11,17 @@ CREATE TABLE IF NOT EXISTS pending_messages (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT OR IGNORE INTO messages (id, content) VALUES 
-    (1, 'Thank you for being you today.'),
+INSERT OR IGNORE INTO messages (id, content) VALUES
+    (1, 'Thank you for being.'),
     (2, 'You are worthy just as you are.'),
-    (3, 'You are enough.');
+    (3, 'You are enough.'),
+    (4, 'Do not believe the lies you tell yourself.'),
+    (5, 'You are loved.'),
+    (6, 'I am proud of you.'),
+    (7, "If you really want something, you will find a way. If you don't, you will find an excuse."),
+    (8, 'What is stopping you?'),
+    (9, 'Love with all your heart.'),
+    (10, 'See the world with eyes of love, not fear.'),
+    (11, 'You are allowed to rest.'),
+    (12, 'I am grateful you are here.'),
+    (13, 'Treat yourself like someone you love.');
