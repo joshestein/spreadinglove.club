353c68103b70975843df55237787d593a6e42120
Josh Stein <josh.e.stein@gmail.com>
2025-11-01 23:05:25 +0200

    Mailer

---
+++ cmd/mailer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	from := os.Getenv("EMAIL_SENDER")
+	recipient := os.Getenv("EMAIL_RECIPIENT")
+	password := os.Getenv("EMAIL_PASSWORD")
+
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := "587"
+
+	to := []string{recipient}
+
+	body := "<html><body>Go to <a href='https://spreadlove.club/admin'>the admin page</a> to check them out.</body></html>"
+	message := []byte("From: " + from + "\n" +
+		"Subject: There are pending love messages for you to review\n" +
+		"MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n" +
+		body + "\n")
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println("Error sending email:", err)
+		return
+	}
+
+	fmt.Println("Email sent successfully!")
+}
